3afcdc04f9cc3812ccb142234b115ff258d0581b
Joey Zhang <joey.zhang@tgitech.net>
2019-07-21 13:09:40 +0800

    Initial Commit

---
+++ ProgressDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProgressDemo
+//
+//  Created by Yi Zhang on 2019/7/21.
+//  Copyright © 2019 Yi Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
